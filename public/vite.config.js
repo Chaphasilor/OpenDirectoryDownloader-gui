3e0fc19a499b623075a1f3d05d9c0287406c2c73
Chaphasilor <ppp.u@web.de>
2021-07-25 20:09:40 +0200

    fix build

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,10 +1,20 @@
-export default {
-  plugins: [],
-  root: `.`,
-  publicDir: `../`, // strange workaround to have access to the `scans` directory through the vite dev server
-  server: {
-    fs: {
-      strict: false,
+export default ({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      // serve specific config
+      root: `.`,
+      plugins: [],
+      publicDir: `../`, // strange workaround to have access to the `scans` directory through the vite dev server
+      server: {
+        fs: {
+          strict: false,
+        }
+      }
+    }
+  } else {
+    return {
+      // build specific config
+      plugins: [],
     }
   }
 }
